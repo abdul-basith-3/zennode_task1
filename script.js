b12ce3373b778dcc6af7cd40f5fe6428816160cb
abdul basith a <abdulbasitha3003@gmail.com>
2024-03-12 08:41:54 +0000

    error corrected2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
+document.getElementById("form").addEventListener("submit", function(event) {
     // Prevent form submission
     event.preventDefault(); 
     // Display success message
     document.getElementById("successMessage").style.display = "block";
-    document.getElementById("myForm").reset();
+    document.getElementById("form").reset();
 });
